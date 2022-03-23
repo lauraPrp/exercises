7a4dbb67dbe8c75f1617f3960924628a5067e222
lauraPrp <laura.porpiglia@gmail.com>
2022-03-23 22:59:36 +0000

    2nd attempt stream/clean code

--- src/main/java/exercises/SalesmanTravelKata.java
+++ src/main/java/exercises/SalesmanTravelKata.java
@@ -1,64 +1,24 @@
 package exercises;
 
-import java.util.*;
 import java.util.stream.Collectors;
 
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class SalesmanTravelKata {
-          public static String travel(String r, String zipcode) {
-          
-        String output = "";
-        String numbers = "";
-        String[] addresses = r.split(",");
-        for (String address : addresses) {
-            String[] words = address.split(" "); //split into words
-            String[] z = zipcode.split(" ");
-            if (z[0].equals(words[words.length-2]) && z[1].equals(words[words.length-1])){
-                if (output.equals("")){
-                    output = output + zipcode + ":";
-                }
-            } else {
-                continue;
-            }
-
-            if (!output.substring(output.length() - 1).equals(":")){
-                output = output + ",";
-            }
-
-            for (int i = 1; i < words.length; i++){
-                if(!isAllUpper(words[i])){
-                    if (output.substring(output.length() - 1).equals(":") 
-                        || output.substring(output.length() - 1).equals(",")){
-                        output = output + words[i];
-                    } else {
-                        output = output + " " + words[i];
-                    }
-                }
-            }
-
-            if (numbers.equals("")){
-                numbers = numbers + words[0];
-            } else {
-                numbers = numbers + "," + words[0];
-            }
+    public static String travel(String r, String zipcode) {
+        if(zipcode.trim().length() < 8){
+            return zipcode + ":/";
         }
-        if (output.equals("")){
-            output = zipcode + ":/";
-        } else {
-            output = output + "/" + numbers;
-        }
-        return output;
-    }
-
-    private static boolean isAllUpper(String s) {
-        for(char c : s.toCharArray()) {
-            if(Character.isLetter(c) && Character.isLowerCase(c)) {
-                return false;
-            }
-        }
-        return true;
+        java.util.List<String> list = java.util.Arrays.stream(r.split(","))
+                .filter(s -> s.contains(zipcode))
+                .map(s -> s.replace(zipcode, ""))
+                .toList();
+        String codes = list.stream()
+                .map(s -> s.split(" ")[0])
+                .collect(Collectors.joining(","));
+        String addresses = list.stream()
+                .map(s -> s.replace(s.split(" ")[0], "").trim())
+                .collect(Collectors.joining(","));
+        return String.format("%s:%s/%s", zipcode, addresses, codes);
     }
 }
 
